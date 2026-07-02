24448825b415e3f59df57a01b8bbba93cfa2ca38
soporanvlad <vlad.soporan@stud.ubbcluj.ro>
2026-07-02 21:01:56 +0300

    Seed initial database data

--- meridian-compass/backend/app/database/init_db.py
+++ meridian-compass/backend/app/database/init_db.py
@@ -1,6 +1,12 @@
-from app.database.database import Base, engine
-
+from app.database.database import Base, engine, SessionLocal
 from app.models import *
+from app.database.seed import seed_database
 
 def init_database():
     Base.metadata.create_all(bind=engine)
+
+    db = SessionLocal()
+    try:
+        seed_database(db)
+    finally:
+        db.close()
